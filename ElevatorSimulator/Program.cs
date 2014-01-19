fd498d5910a0c26dbf4126a6a3305942c960aefa
Jonny Lyon <jl858@york.ac.uk>
2014-01-19 12:34:03 +0000

    Code to demonstrate working car movement
    Just sends the car up and down a few floors using hall calls.  Will
    throw an exception atm because reversing logic is not correct.

--- ElevatorSimulator/Program.cs
+++ ElevatorSimulator/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using ElevatorSimulator.PhysicalDomain;
 using ElevatorSimulator.AbstractDomain;
 using ElevatorSimulator.Agenda;
+using ElevatorSimulator.Calls;
 
 namespace ElevatorSimulator
 {
@@ -16,10 +17,26 @@ namespace ElevatorSimulator
             Car car = new Car();
             shaft.addCar(car);
 
-            car.addHallCall(new Calls.HallCall(1, Direction.Up));
-            car.addHallCall(new Calls.HallCall(5, Direction.Up));
-            car.addHallCall(new Calls.HallCall(3, Direction.Down));
+            car.addHallCall(new HallCall(1, Direction.Up));
+            car.addHallCall(new HallCall(5, Direction.Up));
+            car.addHallCall(new HallCall(3, Direction.Down));
 
+            while (!(Agenda.Agenda.isEmpty() && car.carState.Action == CarAction.Idle))
+            {
+                Console.ReadKey();
+                Console.WriteLine("");
+                car.changeState((Agenda.Agenda.moveToNextEvent() as CarStateChange).getNewCarState());
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Car is now idle.  About to add another call");
+
+            car.addHallCall(new HallCall(6, Direction.Down));
+
+            Console.WriteLine("Call added.  Floor 6, down.");
+
+            // Now an problem will because the logic for reversing is not
+            // correctly defined (yet)
             while (!(Agenda.Agenda.isEmpty() && car.carState.Action == CarAction.Idle))
             {
                 Console.ReadKey();
